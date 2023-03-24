a0095dfcee805ff85d4f3b52828e18ee3c4a7f8d
Lassi Piironen <lsipii@kapsi.fi>
2023-03-24 12:46:23 +0200

    refactor: rearrange responsibilities

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-// @draft: https://github.com/clap-rs/clap
 use anyhow::Result;
-use clap::{command, Parser};
-use clap_complete::Shell;
+use clap::{command, CommandFactory, Parser};
+use clap_complete::{generate, Shell};
+use serde_derive::{Deserialize, Serialize};
+use std::{fs, io};
 
 #[derive(Parser)]
 #[command(name = "VFD-Tools")]
@@ -18,9 +19,33 @@ pub struct CliArguments {
     pub fest: bool,
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Settings {
+    pub profiles: Vec<Profile>,
+    #[serde(rename = "vfd-ssh-git")]
+    pub vfd_ssh_git: String,
+}
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Profile {
+    name: String,
+    services: Vec<String>,
+}
+
 mod runner;
 
 fn main() -> Result<()> {
     let args = CliArguments::parse();
-    runner::Runner::new(args).run()
+
+    if let Some(generator) = args.generator {
+        let mut cmd = CliArguments::command();
+        let name = cmd.get_name().to_string();
+        generate(generator, &mut cmd, name, &mut io::stdout());
+        return Ok(());
+    }
+
+    let setting_file_contents =
+        fs::read_to_string("./settings.json").expect("Failed to read settings.json");
+    let settings: Settings = serde_json::from_str(setting_file_contents.as_str())?;
+
+    runner::Runner::new(args, settings).run()
 }
--- src/runner.rs
+++ src/runner.rs
@@ -1,30 +1,18 @@
-use super::CliArguments;
+use crate::{CliArguments, Settings};
 use anyhow::Result;
-use clap::{Command as ClapCommand, CommandFactory};
-use clap_complete::{generate, Generator};
 use std::process::{Command, Stdio};
-use std::{fs, io};
-
-fn print_completions<G: Generator>(gen: G, cmd: &mut ClapCommand) {
-    generate(gen, cmd, cmd.get_name().to_string(), &mut io::stdout());
-}
 
 pub struct Runner {
     args: CliArguments,
+    settings: Settings,
 }
 
 impl Runner {
-    pub fn new(args: CliArguments) -> Self {
-        Runner { args }
+    pub fn new(args: CliArguments, settings: Settings) -> Self {
+        Runner { args, settings }
     }
 
     pub fn run(&self) -> Result<()> {
-        if let Some(generator) = self.args.generator {
-            let mut cmd = CliArguments::command();
-            print_completions(generator, &mut cmd);
-            return Ok(());
-        }
-
         if self.args.test {
             println!("test");
             let mut command_parts = "docker compose ps".split(' ');
@@ -38,11 +26,7 @@ impl Runner {
 
         if self.args.fest {
             println!("fest");
-            let contents =
-                fs::read_to_string("./settings.json").expect("Failed to read settings.json");
-
-            let configuration: serde_json::Value = serde_json::from_str(contents.as_str())?;
-            println!("{:?}", configuration);
+            println!("{:?}", self.settings);
         }
         Ok(())
     }
