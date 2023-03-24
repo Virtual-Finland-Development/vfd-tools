f3e3d913a68acfea6aeac70decbb3682203eec0d
Lassi Piironen <lsipii@kapsi.fi>
2023-03-24 11:32:50 +0200

    feat: base for runner and auto-completion

--- src/runner.rs
+++ src/runner.rs
@@ -1,5 +1,13 @@
 use super::cli::CliArguments;
 use anyhow::Result;
+use clap::{Command as ClapCommand, CommandFactory};
+use clap_complete::{generate, Generator};
+use std::io;
+use std::process::{Command, Stdio};
+
+fn print_completions<G: Generator>(gen: G, cmd: &mut ClapCommand) {
+    generate(gen, cmd, cmd.get_name().to_string(), &mut io::stdout());
+}
 
 pub struct Runner {
     args: CliArguments,
@@ -11,9 +19,23 @@ impl Runner {
     }
 
     pub fn run(&self) -> Result<()> {
+        if let Some(generator) = self.args.generator {
+            let mut cmd = CliArguments::command();
+            print_completions(generator, &mut cmd);
+            return Ok(());
+        }
+
         if self.args.test {
             println!("test");
+            let mut command_parts = "docker compose ps".split(' ');
+            let primary_command = command_parts.next().unwrap();
+            Command::new(primary_command)
+                .args(command_parts)
+                .stdout(Stdio::inherit())
+                .spawn()
+                .expect("Failed to execute docker compose");
         }
+
         if self.args.fest {
             println!("fest");
         }
