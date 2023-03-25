3ead349d79eca4073607ea1ea254649d61dca262
Lassi Piironen <lsipii@kapsi.fi>
2023-03-25 17:56:44 +0200

    feat: composing the command struct

--- src/runner.rs
+++ src/runner.rs
@@ -1,16 +1,22 @@
 use crate::{settings::Settings, CliArguments};
 use anyhow::Result;
-use std::process::{Command, Stdio};
+use std::{
+    env,
+    process::{Command, Stdio},
+};
 
 pub fn run(args: CliArguments, settings: Settings) -> Result<()> {
     if args.test {
         println!("test");
+        let project_root_path = settings.project_root_path.clone();
+        let formatted_runner_path = format_runner_path(project_root_path.clone());
         for profile in settings.profiles.iter() {
-            println!("Running command for profile: {}", profile.name);
+            println!("----- Profile: {} ...", profile.name);
             for service in profile.services.iter() {
+                println!("> {}{}:\ndocker compose ps", formatted_runner_path, service);
                 run_command(&format!(
-                    "docker compose -f ../{}/docker-compose.yml ps",
-                    service
+                    "docker compose -f {}/{}/docker-compose.yml ps",
+                    project_root_path, service
                 ))?;
             }
         }
@@ -33,4 +39,18 @@ fn run_command(command: &str) -> Result<()> {
         .spawn()?
         .wait()?;
     Ok(())
+}
+
+fn format_runner_path(project_root_path: String) -> String {
+    let mut runner_path = project_root_path.clone();
+    if env::var("HOME").is_ok() {
+        let home_path = env::var("HOME").unwrap();
+        if runner_path.starts_with(home_path.as_str()) {
+            runner_path = runner_path.replace(home_path.as_str(), "~").to_string();
+        }
+    }
+    if runner_path.contains("/../") {
+        runner_path = runner_path.replace("/../", "/").to_string();
+    }
+    runner_path
 }
--- src/settings.rs
+++ src/settings.rs
@@ -1,8 +1,11 @@
 use serde_derive::{Deserialize, Serialize};
-use std::fs;
+use std::{env, fs};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Settings {
+    #[serde(rename = "project-root-path")]
+    #[serde(default)]
+    pub project_root_path: String,
     pub profiles: Vec<Profile>,
     #[serde(rename = "vfd-ssh-git")]
     pub vfd_ssh_git: String,
@@ -14,7 +17,30 @@ pub struct Profile {
 }
 
 pub fn get_settings() -> Settings {
+    // Read settings.json
     let setting_file_contents =
         fs::read_to_string("./settings.json").expect("Failed to read settings.json");
-    serde_json::from_str(setting_file_contents.as_str()).expect("Failed to parse settings.json")
+    let mut settings: Settings = serde_json::from_str(setting_file_contents.as_str())
+        .expect("Failed to parse settings.json");
+
+    // Resolve projects root path
+    if env::var("VFD_PROJECTS_ROOT").is_ok() {
+        settings.project_root_path = env::var("VFD_PROJECTS_ROOT").unwrap();
+    }
+    // If projects path is not set, use the parent directory of the vfd-tools project directory
+    if settings.project_root_path.is_empty() {
+        settings.project_root_path = std::env::current_dir()
+            .unwrap()
+            .to_str()
+            .unwrap()
+            .to_string()
+            + "/../";
+    }
+
+    // Resolve other matters
+    if env::var("VFD_SSH_GIT").is_ok() {
+        settings.vfd_ssh_git = env::var("VFD_SSH_GIT").unwrap();
+    }
+
+    settings
 }
