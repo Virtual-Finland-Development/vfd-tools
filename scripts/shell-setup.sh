4deba414fb147102c679299ef6dec85759270ff9
Lassi Piironen <lsipii@kapsi.fi>
2023-04-17 18:22:09 +0300

    fix: shell completion required a non-symlink reference

--- scripts/shell-setup.sh
+++ scripts/shell-setup.sh
@@ -1,5 +1,5 @@
 SHELL=$(basename $SHELL)
 SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-alias vfd="${SCRIPT_PATH}/vfd.sh"
+alias vfd="${SCRIPT_PATH}/../bin/vfd"
 source ${SCRIPT_PATH}/generated/autocomplete.${SHELL} 2>/dev/null || echo "> No autocomplete script for ${SHELL}"
