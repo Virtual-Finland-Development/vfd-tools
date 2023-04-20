1c5faa66fc513ec86659ad32e2483f9282b92f6b
Lassi Piironen <lsipii@kapsi.fi>
2023-04-20 14:08:16 +0300

    fix: shell resolving for mac

--- scripts/shell-setup.sh
+++ scripts/shell-setup.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
-current_shell=$(ps -p $$ -o comm=)
-current_shell_short=$(basename ${current_shell})
+current_shell=$(ps -p $$ -o comm= | tr -d '-')
 
 script_path=${BASH_SOURCE[0]}
-if [ "${current_shell_short}" = "zsh" ]; then
+if [ "${current_shell}" = "zsh" ]; then
     script_path=${0:A}
 fi
 
 PROJECT_PATH=$(dirname "$(dirname "$(readlink -f "${script_path}")")")
-source ${PROJECT_PATH}/scripts/generated/autocomplete.${current_shell_short} 2>/dev/null || echo "> No autocomplete script for ${current_shell_short}"
+source ${PROJECT_PATH}/scripts/generated/autocomplete.${current_shell} 2>/dev/null || echo "> No autocomplete script for ${current_shell}"
 
 function vfd() {
     make --silent --directory ${PROJECT_PATH} install
