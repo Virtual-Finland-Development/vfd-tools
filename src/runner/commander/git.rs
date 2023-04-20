0e323651ac7b793d4166f5e5065a410dc4951f21
Lassi Piironen <lsipii@kapsi.fi>
2023-04-20 18:39:26 +0300

    fix: git command log bad

--- src/runner/commander/git.rs
+++ src/runner/commander/git.rs
@@ -4,7 +4,7 @@ pub fn run_action(settings: &Settings, service: &str, command: &str) {
     let projects_root_path = settings.projects_root_path.clone();
     let formatted_runner_path = utils::format_runner_path(projects_root_path.clone());
 
-    println!("> {} → git {}", formatted_runner_path, command);
+    println!("> {}/{} → git {}", formatted_runner_path, service, command);
     utils::run_command(
         &format!("git -C {}/{} {}", projects_root_path, service, command),
         false,
